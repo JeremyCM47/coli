1c156980a87366cbb2d0f2bc44b18779bf098709
Jérémy <jerry.c-m@live.fr>
2022-04-22 13:36:57 +0200

    wip update product

--- src/app/admin/update/update.component.ts
+++ src/app/admin/update/update.component.ts
@@ -1,4 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ProductService} from "../../product/product.service";
 
 @Component({
   selector: 'app-update',
@@ -7,19 +9,35 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  closeModal() {
-    this.displayStyleChange.emit("none");
-  }
-
   @Input() product;
   @Input() displayStyle;
 
   @Output() displayStyleChange = new EventEmitter<string>();
+
+  updateProductForm: FormGroup;
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.updateProductForm = new FormGroup({
+      name: new FormControl('',[Validators.required]),
+      price: new FormControl('', [Validators.required]),
+      description: new FormControl('',[Validators.required]),
+      imageUrl: new FormControl('',[Validators.required])
+    });
+    setTimeout(()=> {
+      console.log(this.product);
+    },3000)
+  }
+
+  onSubmit() {
+    this.productService.updateProduct(this.updateProductForm.value);
+    this.closeModal();
+  }
+  closeModal() {
+    this.displayStyleChange.emit("none");
+  }
+
+
 
 
 
